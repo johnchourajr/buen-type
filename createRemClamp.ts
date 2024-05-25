d3e3107eeb8a86eeebd6f5edf9137c86c092873f
johnchourajr <hi@john.design>
2024-05-24 23:25:26 -0700

    first commit

---
+++ createRemClamp.ts
@@ -0,0 +1,19 @@
+/**
+ *
+ * @param minFontSize - The minimum font size in rem
+ * @param maxFontSize - The maximum font size in rem
+ * @param minScreenSize - The minimum screen size in pixels
+ * @param maxScreenSize - The maximum screen size in pixels */
+export function createRemClamp(
+  minFontSize: number,
+  maxFontSize: number,
+  minScreenSize: number = 1024,
+  maxScreenSize: number = 1440
+): string {
+  const minWidth = minScreenSize / 16;
+  const maxWidth = maxScreenSize / 16;
+  const m = (maxFontSize - minFontSize) / (maxWidth - minWidth);
+  const b = -minWidth * m + minFontSize;
+
+  return `clamp(${minFontSize}rem, ${b}rem + ${m * 100}vw, ${maxFontSize}rem)`;
+}
---
+++ mod.ts
@@ -0,0 +1,14 @@
+/**
+ * A module that provides a function to create a `rem`-based `clamp` function.
+ *
+ * @example
+ * ```ts
+ * import { createRemClamp } from "@buen/type";
+ *
+ * createRemClamp(1, 2, 1024, 1440); // "clamp(1rem, 1rem + 0.06666666666666667vw, 2rem)"
+ * ```
+ *
+ * @module
+ */
+
+export * from "./createRemClamp.ts";
