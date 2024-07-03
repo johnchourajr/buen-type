0b4848b740f8f096fca4093270cb8ef8f4d212cf
johnchourajr <hi@john.design>
2024-07-03 00:33:57 -0700

    fix: footer on iframe page

--- example/src/components/GlobalFooter/GlobalFooter.tsx
+++ example/src/components/GlobalFooter/GlobalFooter.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function GlobalFooter() {
+  const path = usePathname();
+
+  if (path === "/scale") {
+    return null;
+  }
   return (
     <footer className="p-4">
       Buenos dias, © 2024{" "}
