9c640c0117858a41035312e69c9d81c4a97bbd1c
johnchourajr <hi@john.design>
2024-08-18 12:46:59 -0700

    fix: favicon

--- example/src/app/layout.tsx
+++ example/src/app/layout.tsx
@@ -26,9 +26,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg" sizes="32x32" />
-      </head>
       <body className="font-sans">
         <GlobalHeader />
         {children}
