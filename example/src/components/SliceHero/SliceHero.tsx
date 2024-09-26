a7afbcb17d5d7cfb47be34a0ef0ad8be25824004
johnchourajr <hi@john.design>
2024-09-25 23:07:02 -0700

    update hero

--- example/src/components/SliceHero/SliceHero.tsx
+++ example/src/components/SliceHero/SliceHero.tsx
@@ -3,7 +3,7 @@ import { SvgHeader } from "../SvgHeader";
 export function SliceHero() {
   return (
     <>
-      <section className="col-span-full flex items-center justify-center h-[43vw] min-h-[50vh] mt-40 md:mt-0">
+      <section className="col-span-full flex flex-col items-center justify-end h-[43vw] min-h-[50vh] mt-40 md:mt-0">
         <SvgHeader className="w-[calc(100%+32px)] -mx-4 -mt-0.25em" />
       </section>
     </>
